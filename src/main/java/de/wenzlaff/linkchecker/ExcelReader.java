8bcb28f4be631dedc16cc90af9181bbb3c638045
Thomas Wenzlaff <karotz@wenzlaff.de>
2018-05-13 17:50:41 +0200

    Neuer Excel Reader von Apache

---
+++ src/main/java/de/wenzlaff/linkchecker/ExcelReader.java
@@ -0,0 +1,64 @@
+package de.wenzlaff.linkchecker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+
+/**
+ * Basis Excel Reader für CSV Dateien.
+ * 
+ * <pre>
+          <dependency>
+		    <groupId>org.apache.commons</groupId>
+		    <artifactId>commons-csv</artifactId>
+		    <version>1.5</version>
+		</dependency>
+ * </pre>
+ * 
+ * @author Thomas Wenzlaff
+ *
+ */
+public class ExcelReader {
+
+	private CSVParser parser;
+
+	/**
+	 * Liest die Excel Datei ein.
+	 * 
+	 * @param uri
+	 *            die File URI
+	 * @return der CSVParser
+	 * @throws IOException
+	 */
+	public CSVParser read(URI uri) throws IOException {
+
+		BufferedReader reader = Files.newBufferedReader(Paths.get(uri));
+
+		parser = new CSVParser(reader, CSVFormat.EXCEL.withDelimiter(';').withHeader().withIgnoreHeaderCase().withTrim());
+
+		return parser;
+	}
+
+	public void close() {
+		try {
+			parser.close();
+		} catch (IOException e) {
+			System.err.println("Konnte Parser nicht schließen. " + e.getMessage());
+		}
+	}
+
+	@Override
+	public String toString() {
+		try {
+			return "BasicCsvReader [Zeilen= " + parser.getRecords().size() + " Titel=" + parser.getHeaderMap().keySet() + "]";
+		} catch (IOException e) {
+			return "IOException " + e.getMessage();
+		}
+	}
+
+}
---
+++ src/test/java/de/wenzlaff/linkchecker/ExcelReaderTest.java
@@ -0,0 +1,58 @@
+package de.wenzlaff.linkchecker;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.junit.Test;
+
+/**
+ * Testklasse für den Excelreader.
+ * 
+ * @author Thomas Wenzlaff
+ *
+ */
+public class ExcelReaderTest {
+
+	@Test
+	public void testRead() throws Exception {
+
+		ExcelReader reader = new ExcelReader();
+
+		File f = new File("src/test/resources/de/wenzlaff/linkchecker/test.csv");
+
+		CSVParser csvParser = reader.read(f.toURI());
+
+		System.out.println("Parser: " + csvParser);
+
+		printRecords(csvParser);
+
+		System.out.println("Parser: " + reader);
+
+		reader.close();
+	}
+
+	private void printRecords(CSVParser csvParser) {
+		for (CSVRecord csvRecord : csvParser) {
+
+			// Zugriff über Index
+			String id = csvRecord.get(0);
+
+			// Zugriff über Titel Namen
+			String name = csvRecord.get("Name");
+
+			// Test Name über Titel und Index
+			assertEquals(name, csvRecord.get(2));
+
+			// Printing the record
+			System.out.println("Record Number - " + csvRecord.getRecordNumber());
+			System.out.println("Name : " + name);
+			System.out.println("Nr.  : " + id);
+			System.out.println("\n");
+			System.out.println(csvRecord);
+		}
+	}
+
+}
