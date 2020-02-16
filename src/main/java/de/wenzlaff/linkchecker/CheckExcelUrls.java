0516eadabefbedd052d6294bd9a12a7717eb018e
Thomas Wenzlaff <karotz@wenzlaff.de>
2020-02-16 17:37:17 +0100

    Dep. entfernt

--- src/main/java/de/wenzlaff/linkchecker/CheckExcelUrls.java
+++ src/main/java/de/wenzlaff/linkchecker/CheckExcelUrls.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-import org.apache.poi.hssf.util.CellReference;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
